d86ed3719360ca8f55c92c9f43ef6ede18cb781f
Kevin-C-Silva <kevin.silva158@etec.sp.gov.br>
2025-08-07 14:21:15 -0300

    Adicionando pasta Data e classe Database

--- ProjetoOlimpiadas/obj/Debug/net8.0/ProjetoOlimpiadas.AssemblyInfo.cs
+++ ProjetoOlimpiadas/obj/Debug/net8.0/ProjetoOlimpiadas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoOlimpiadas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d1b632ba72f47dd0f9376bf6947036d6b9ce44")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoOlimpiadas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoOlimpiadas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
